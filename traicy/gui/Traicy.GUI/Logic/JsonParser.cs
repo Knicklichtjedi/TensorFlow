468cb92dacf317eb0ffc33539323e8eb4f8e1527
TraneQ28 <eva.stepinski@gmail.com>
2018-03-28 01:04:05 +0200

    gitignore and GUI project structure changed; json parser class and setting classes added
    +CHANGED: .gitignore file for ignoring nuget packages and user specific files
    +CHANGED: project structure for visual studio solution
    +ADDED: Newstonsoft.Json and IronPython Nuget-Packages to GUI project
    +ADDED: generic JSON-parser class for parsing settings
    +ADDED: setting properties class and interface

---
+++ traicy/gui/Traicy.GUI/Logic/JsonParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Traicy.GUI.Logic
+{
+    /// <summary>
+    /// Class for parsing generic details for shortcuts and programinformation
+    /// </summary>
+    public class JsonParser
+    {
+        /// <summary>
+        /// Method that generically serializes list of objects into JSON by using the given filepath.
+        /// </summary>
+        /// <typeparam name="T">Generic type for serialization</typeparam>
+        /// <param name="listOfInformation">Generic (list of) objects </param>
+        /// <param name="filePath">File destination of the JSON-File</param>
+        public static void SerializeToJson<T>(T listOfInformation, string filePath)
+        {
+            try
+            {
+                string jsonString = JsonConvert.SerializeObject(listOfInformation, Formatting.Indented);
+                File.WriteAllText(filePath, jsonString);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method that generically deserializes list of objects from JSON by using the given filepath.
+        /// </summary>
+        /// <typeparam name="T">Generic type for deserialization</typeparam>
+        /// <param name="filePath">File destination of the JSON-File</param>
+        /// <returns>Returns generic object that has been deserialized</returns>
+        public static T DeserializeFromJson<T>(string filePath)
+        {
+            T listOfInformation = default(T);
+
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                listOfInformation = JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return listOfInformation;
+        }
+    }
+}
