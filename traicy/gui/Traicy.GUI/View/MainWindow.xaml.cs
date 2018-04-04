8ae7f0f75f3df0830c4adb5d778df413c40dd802
TraneQ28 <eva.stepinski@gmail.com>
2018-04-04 02:42:24 +0200

    Minor GUI changes and text-to-speech functionality added
    +ADDED: resources-folder for images, icons or other gui elements
    +ADDED: Traicy logo and settings icon to resources folder
    +ADDED: Settings window and settings user control for modifying project and gui setting configurations
    +ADDED: text-to-speech functionality when clicking button "start object detection!
     (for testing purposes)
    +ADDED: Button Eventhandler in MainWindow

--- traicy/gui/Traicy.GUI/View/MainWindow.xaml.cs
+++ traicy/gui/Traicy.GUI/View/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
+using System.Speech.Synthesis;
 
 namespace Traicy.GUI.View
 {
@@ -10,6 +13,8 @@ namespace Traicy.GUI.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static SpeechSynthesizer _speaker;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,6 +25,56 @@ namespace Traicy.GUI.View
             //dynamic loadIPython = ironPythonRuntime.UseFile(@"../filters/ImageFilter.py");
             //loadIPython.MethodCall("main");
 
+        }
+
+        private void ButtonSettings_OnClick(object sender, RoutedEventArgs e)
+        {
+            new SettingsWindow().ShowDialog();
+
+
+        }
+
+        //zusätzliche Methode, kann manchmal nützlich sein
+        private static List<VoiceInfo> GetInstalledVoices()
+        {
+            var listOfVoiceInfo = from voice
+                    in _speaker.GetInstalledVoices()
+                select voice.VoiceInfo;
+
+            return listOfVoiceInfo.ToList<VoiceInfo>();
+        }
+
+        private void ButtonStartObjectDetection_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextToSpeechTest();
+        }
+
+        private static void TextToSpeechTest()
+        {
+            _speaker = new SpeechSynthesizer();
+
+            //
+            var voices = GetInstalledVoices();
+
+            //In dem Fall unnötig, aber falls zB vorher OutputToWav eingestellt war
+            _speaker.SetOutputToDefaultAudioDevice();
+            //Geschwindigkeit (-10 - 10)
+            _speaker.Rate = 1;
+            //Lautstärke (0-100)
+            _speaker.Volume = 100;
+            _speaker.SelectVoice("Microsoft Zira Desktop");
+            //Such passende Stimme zu angegebenen Argumenten
+            //_speaker.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen);
+            //Text wird ausgegeben (abbrechen mit speaker.CancelAsync())
+            //_speaker.SpeakAsync("a ist der Buchstabe!");
+            _speaker.SpeakAsync("I see an T!");
+            _speaker.SpeakAsync("I see an R!");
+            _speaker.SpeakAsync("I see an A!");
+            _speaker.SpeakAsync("I see an I!");
+            _speaker.SpeakAsync("I see an C!");
+            _speaker.SpeakAsync("I see an Y!");
+            _speaker.SpeakAsync("T R A I C Y");
+            _speaker.SpeakAsync("TRAICY");
         }
     }
 }
---
+++ traicy/gui/Traicy.GUI/View/SettingsControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Traicy.GUI.View
+{
+    /// <summary>
+    /// Interaktionslogik für SettingsControl.xaml
+    /// </summary>
+    public partial class SettingsControl : UserControl
+    {
+        public SettingsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleSpeechButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender != null)
+            {
+                if (sender is ToggleButton button)
+                {
+                    button.Content = button.Content.ToString() == "An" ? "Aus" : "An";
+                }
+
+                //TODO: Sprachausgabe an- und ausschalten
+            }
+        }
+    }
+}
