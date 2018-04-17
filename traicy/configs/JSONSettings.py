6305e1da3554060af6f6cf65391a4410d3f06598
Dennis Lüdecke <dennis.lue@t-online.de>
2018-04-17 21:40:16 +0200

    Updated json file and added json reading to ImageFilter
    ADDED: new json values in json-file
    ADDED: new values (I/O) in JSONSettings
    ADDED: Assigning values method in ImageFilter
    ADDED: relative path to json file in ImageFilter
    CHANGED: changed some variable names to match the naming convention
    CHANGED: Some visual improvements in JSONSettings for better readability

--- traicy/configs/JSONSettings.py
+++ traicy/configs/JSONSettings.py
@@ -5,12 +5,20 @@ import json
 import codecs
 
 __imported__ = False
+
 __i_dim__ = 0
 __i_dim_s__ = 0
 __i_border__ = 0
+
 __f_canny__ = 0.0
 __f_bin_gauss__ = 0.0
 __f_bin_thresh__ = 0.0
+
+__f_green_low__ = 0.0
+__f_green_high__ = 0.0
+
+__f_green_saturation__ = 0.0
+__f_green_brightness__ = 0.0
 
 __data__ = {}
 
@@ -23,6 +31,12 @@ class JSONValues (Enum):
     FILTER_CANNY = 3
     FILTER_BIN_GAUSS = 4
     FILTER_BIN_THRESHOLD = 5
+
+    FILTER_GREEN_LOW = 6
+    FILTER_GREEN_HIGH = 7
+
+    FILTER_GREEN_SATURATION = 8
+    FILTER_GREEN_BRIGHTNESS = 9
 
 
 def parse_data(filename):
@@ -37,7 +51,10 @@ def parse_data(filename):
         if data is not None:
 
             # Change variable access to global
-            global __i_dim__, __i_dim_s__, __i_border__, __f_canny__, __f_bin_gauss__, __f_bin_thresh__, __imported__
+            global __i_dim__, __i_dim_s__, __i_border__
+            global __f_canny__, __f_bin_gauss__, __f_bin_thresh__, __f_green_low__, __f_green_high__, \
+                __f_green_saturation__, __f_green_brightness__
+            global __imported__
 
             image_access = data['image']
             filter_access = data['filter']
@@ -50,6 +67,12 @@ def parse_data(filename):
             __f_bin_gauss__ = filter_access['binary_gauss']
             __f_bin_thresh__ = filter_access['binary_threshold']
 
+            __f_green_low__ = filter_access['green_low']
+            __f_green_high__ = filter_access['green_high']
+
+            __f_green_saturation__ = filter_access['green_saturation']
+            __f_green_brightness__ = filter_access['green_brightness']
+
             __imported__ = True
 
     data_file.close()
@@ -57,18 +80,29 @@ def parse_data(filename):
 
 def get_data(json_value, *filename):
     if __imported__:
+
         if json_value == JSONValues.IMAGE_DIMENSION:
             return __i_dim__
         if json_value == JSONValues.IMAGE_DIMENSION_SMALL:
             return __i_dim_s__
         if json_value == JSONValues.IMAGE_BORDER:
             return __i_border__
+
         if json_value == JSONValues.FILTER_CANNY:
             return __f_canny__
         if json_value == JSONValues.FILTER_BIN_GAUSS:
             return __f_bin_gauss__
         if json_value == JSONValues.FILTER_BIN_THRESHOLD:
             return __f_bin_thresh__
+        if json_value == JSONValues.FILTER_GREEN_LOW:
+            return __f_green_low__
+        if json_value == JSONValues.FILTER_GREEN_HIGH:
+            return __f_green_high__
+        if json_value == JSONValues.FILTER_GREEN_SATURATION:
+            return __f_green_saturation__
+        if json_value == JSONValues.FILTER_GREEN_BRIGHTNESS:
+            return __f_green_brightness__
+
     else:
         if filename is not None or filename != "":
             parse_data(filename)
@@ -81,7 +115,9 @@ def write_data(filename, json_value, value):
     if not __imported__:
         parse_data(filename)
 
-    global __i_dim__, __i_dim_s__, __i_border__, __f_canny__, __f_bin_gauss__, __f_bin_thresh__
+    global __i_dim__, __i_dim_s__, __i_border__
+    global __f_canny__, __f_bin_gauss__, __f_bin_thresh__, __f_green_low__, __f_green_high__, \
+        __f_green_saturation__, __f_green_brightness__
 
     if json_value == JSONValues.IMAGE_DIMENSION:
         __data__['image']['dimension'] = value
@@ -95,6 +131,8 @@ def write_data(filename, json_value, value):
         __data__['image']['border'] = value
         __i_border__ = value
 
+    ##################################################
+
     if json_value == JSONValues.FILTER_CANNY:
         __data__['filter']['canny'] = value
         __f_canny__ = value
@@ -106,6 +144,24 @@ def write_data(filename, json_value, value):
     if json_value == JSONValues.FILTER_BIN_THRESHOLD:
         __data__['filter']['binary_threshold'] = value
         __f_bin_thresh__ = value
+
+    ##################################################
+
+    if json_value == JSONValues.FILTER_BIN_THRESHOLD:
+        __data__['filter']['green_low'] = value
+        __f_green_low__ = value
+
+    if json_value == JSONValues.FILTER_BIN_THRESHOLD:
+        __data__['filter']['green_high'] = value
+        __f_green_high__ = value
+
+    if json_value == JSONValues.FILTER_BIN_THRESHOLD:
+        __data__['filter']['green_saturation'] = value
+        __f_green_saturation__ = value
+
+    if json_value == JSONValues.FILTER_BIN_THRESHOLD:
+        __data__['filter']['green_brightness'] = value
+        __f_green_brightness__ = value
 
     with open(filename, 'wb') as outfile:
         json.dump(__data__, codecs.getwriter('utf-8')(outfile), indent=4, ensure_ascii=False)
